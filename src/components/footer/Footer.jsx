fbfd8853b86c86fae9ab859cf1348459ef3cc116
Sunatillo <madazimovsunnatillo02@gmail.com>
2023-02-27 11:02:31 +0300

    name

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -13,10 +13,10 @@ const Footer = () => {
             <div className="footer__wrapper">
                 <ul className="social">
                     <li className="social__item"><a href="#!"><img src={vk} alt="Link"/></a></li>
-                    <li className="social__item"><a href="#!"><img src={instagram} alt="Link"/></a></li>
+                    <li className="social__item"><a href="https://www.instagram.com/" target='_blank'><img src={instagram} alt="Link"/></a></li>
                     <li className="social__item"><a href="#!"><img src={twitter} alt="Link"/></a></li>
-                    <li className="social__item"><a href="#!"><img src={gitHub} alt="Link"/></a></li>
-                    <li className="social__item"><a href="#!"><img src={linkedin} alt="Link"/></a></li>
+                    <li className="social__item"><a href="https://www.github.com/" target='_blank'><img src={gitHub} alt="Link"/></a></li>
+                    <li className="social__item"><a href="https://www.linkedin.com/" target='_blank'><img src={linkedin} alt="Link"/></a></li>
                 </ul>
                 <div className="copyright">
                     <p>© 2022 frontend-dev.com</p>
--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -9,15 +9,15 @@ const Contact = () => {
             <ul className="content-list">
                 <li className="content-list__item">
                     <h2 className="title-2">Location</h2>
-                    <p>Moscow, Russia</p>
+                    <p>Uzbekistan, Tashkent</p>
                 </li>
                 <li className="content-list__item">
                     <h2 className="title-2">Telegram / WhatsApp</h2>
-                    <p><a href="tel:+79051234567">+7 (905) 123-45-67</a></p>
+                    <p><a href="tel:+998990853558">+998 99 085 35 58</a></p>
                 </li>
                 <li className="content-list__item">
                     <h2 className="title-2">Email</h2>
-                    <p><a href="mailto:webdev@protonmail.com">webdev@protonmail.com</a></p>
+                    <p><a href="mailto:madazimovsunnatillo02@gmail.com">madazimovsunnatillo02@gmail.com</a></p>
                 </li>
             </ul>
 
