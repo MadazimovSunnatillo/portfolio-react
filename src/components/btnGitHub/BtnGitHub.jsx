e2173e1b512298a0caccbaaa613e8d3664942a12
Sunatillo <madazimovsunnatillo02@gmail.com>
2023-02-27 10:37:52 +0300

    my first project in react

---
+++ src/components/btnGitHub/BtnGitHub.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import './style.css'
+import gitHub from './gitHub-black.svg';
+
+const BtnGitHub = ({link}) => {
+  return (
+        <a href={link} target="_blank" rel='noreferrer' className="btn-outline">
+            <img src={gitHub} alt=""/>
+            GitHub repo
+        </a>
+  )
+}
+
+export default BtnGitHub
